c616d08663cb61412b5be3fb885167366f970a72
Caden Gudenkauf <cjgud882@learner.dvusd.org>
2022-08-18 11:25:56 -0700

    added two classes

---
+++ Java/CodeHS.java
@@ -0,0 +1,11 @@
+public class CodeHS
+{
+    final static int max = 8;
+    
+    public static void Main(String args[]) {
+        
+        System.out.println(max);
+        
+    }
+    
+}
---
+++ Java/quiz.java
@@ -0,0 +1,38 @@
+
+
+public class quiz
+{
+    String response1;
+    String response2;
+    String response3;
+    
+    
+    public static void main(String args[]) {
+    System.out.println("What is 1+1?"); //
+       System.out.println("--------------");
+       System.out.println("a) - 1");
+        System.out.println("b) - 2");
+         System.out.println("c) - 3");
+          System.out.println("d) - 4");
+          
+          
+          
+       System.out.println("What is the capital of Arizona?"); //
+        System.out.println("--------------");
+        System.out.println("a) - Phoenix");
+         System.out.println("b) - Washington DC");
+          System.out.println("c) - Berlin");
+           System.out.println("d) - Helsinki");
+       System.out.println("Which is the proper spelling?"); //
+       System.out.println("--------------");
+       System.out.println("a) - Recuring");
+       System.out.println("b) - Reccuring");
+       System.out.println("c) - Reccurring");
+       System.out.println("d) - Recurring");
+       
+    
+    }
+    
+    
+    
+}
